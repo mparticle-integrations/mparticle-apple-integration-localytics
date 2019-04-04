b49adc69aacc27bbafa8498c41093392bf6df905
Peter Jenkins <pjenkins@mparticle.com>
2019-04-04 10:48:20 -0400

    Fix linter warning

--- mParticle-Localytics/MPKitLocalytics.m
+++ mParticle-Localytics/MPKitLocalytics.m
@@ -235,7 +235,7 @@
 #endif
 
 #if TARGET_OS_IOS == 1 && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-- (nonnull MPKitExecStatus *)userNotificationCenter:(nonnull UNUserNotificationCenter *)center didReceiveNotificationResponse:(nonnull UNNotificationResponse *)response {
+- (nonnull MPKitExecStatus *)userNotificationCenter:(nonnull UNUserNotificationCenter *)center didReceiveNotificationResponse:(nonnull UNNotificationResponse *)response  API_AVAILABLE(ios(10.0)){
     [Localytics didReceiveNotificationResponseWithUserInfo:response.notification.request.content.userInfo];
 
     MPKitExecStatus *execStatus = [[MPKitExecStatus alloc] initWithSDKCode:@(MPKitInstanceLocalytics) returnCode:MPKitReturnCodeSuccess];
